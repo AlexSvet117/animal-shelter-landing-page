8f025f952d8dbba2d33113be6834f4a0e8fb8add
AlexSvet117 <alex.svet117@gmail.com>
2025-01-22 18:44:29 +0000

    WIP all

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+Footer
+
+function Footer() {
+  return (
+    <div className="text-bg-primary mt-3 p-3">
+      <p className="text-center text-body-light">@Made in 2025</p>
+    </div>
+  )
+}
+
+export default Footer
