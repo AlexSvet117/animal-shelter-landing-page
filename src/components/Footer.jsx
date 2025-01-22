dcdb98e6996a1f510e2ecfe2b8c5e1232b18f92c
AlexSvet117 <alex.svet117@gmail.com>
2025-01-22 19:07:37 +0000

    Footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ Footer
 function Footer() {
   return (
     <div className="text-bg-primary mt-3 p-3">
-      <p className="text-center text-body-light">@Made in 2025</p>
+      <p className="text-center text-body-light">@ Animal Shelter 2025. All Rights Reserved</p>
     </div>
   )
 }
