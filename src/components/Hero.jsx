3b6cd655f97e636ea198be4dd2879b691e180433
AlexSvet117 <alex.svet117@gmail.com>
2025-01-23 18:03:41 +0000

    Hero component update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,21 @@
 function Hero() {
     return (
-  <div className="container col-xxl-8 px-4 py-5">
+  <div className="container col-xxl-9 px-4 py-5">
     <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
       <div className="col-10 col-sm-8 col-lg-6">
-        <img src="bootstrap-themes.png" className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/>
+        <img src="/src/images/animals.png" className="d-block mx-lg-auto img-fluid rounded-3" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/>
       </div>
       <div className="col-lg-6">
-        <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Responsive left-aligned hero with image</h1>
-        <p className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+        <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Find Your Perfect</h1>
+        <h1 className="display-5 fw-bold text-primary lh-1 mb-3">Companion</h1>
+        <p className="lead">Every pet in our shelter has a story and is waiting to be taken home. Start your journey of pet adoption today and bring home joy and happiness.</p>
         <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-          <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-          <button type="button" className="btn btn-outline-secondary btn-lg px-4">Default</button>
+          <button type="button" className="btn btn-primary btn-lg rounded-5 px-4 me-md-2">Adopt a Pet</button>
+          <button type="button" className="btn btn-outline-secondary border-primary text-primary rounded-5 btn-lg px-4">Learn More</button>
         </div>
       </div>
     </div>
   </div>
-      // <div className="bg-light text-dark py-5">
-      //   <div className="container text-center">
-      //     <h1 className="display-2">Welcome to My Website!</h1>
-      //     <p className="lead">
-      //         Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat illo est ut laborum debitis quasi atque quis officiis adipisci repellendus.
-      //     </p>
-      //     <button className="btn btn-primary">Learn More</button>
-      //   </div>
-      // </div>
 
       
 
